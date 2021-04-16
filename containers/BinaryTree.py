03e1aa9803d43ee8bb1826117376bed684dea0d2
Nishka <Nishka.Ayyar.23@cmc.edu>
2021-04-16 15:30:33 -0700

    Binary Number 2

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -78,8 +78,8 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder_print(self.root, '')
         else:
-            raise ValueError('Traversal type ' + str(traversal_type)
-                             + ' is not supported.')
+            raise ValueError('Traversal type ' + str(traversal_type) +
+                             ' is not supported.')
 
     def preorder_print(self, start, traversal):
         '''
@@ -138,8 +138,8 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder(self.root, [])
         else:
-            return ValueError('Traversal Type ' + str(traversal_type)
-                              + ' is not supported.')
+            return ValueError('Traversal Type ' + str(traversal_type) +
+                              ' is not supported.')
 
     def preorder(self, start, traversal):
         '''
